06033779162b25da70b09d74c165b4a8594719fc
Dolores DIAZ <diaz.dolores@gmail.com>
2022-04-04 22:44:30 +0200

    ecriture du test bug n2

--- server.py
+++ server.py
@@ -17,67 +17,64 @@ def load_competitions():
 competitions = load_competitions()
 clubs = load_clubs()
 
+app = Flask(__name__)
+app.secret_key = 'something_special'
 
-def create_app(config):
-    app = Flask(__name__)
-    app.secret_key = 'something_special'
-    app.config["TESTING"] = config.get("TESTING")
 
-    @app.route('/')
-    def index():
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+@app.route('/show_summary', methods=['POST'])
+def show_summary():
+    """ function that permits the authentification of a user"""
+    if request.form['email'] == "":
+        flash("Email vide, veuillez ressaisir un email")
+        return render_template('index.html')
+    clubs_list = []
+    for club in clubs:
+        if club['email'] == request.form['email']:
+            clubs_list.append(club)
+            return render_template(
+                'welcome.html',
+                club=club,
+                competitions=competitions)
+    if clubs_list == []:
+        flash('Utilisateur non reconnu')
         return render_template('index.html')
 
-    @app.route('/show_summary', methods=['POST'])
-    def show_summary():
-        """ function that permits the authentification of a user"""
-        if request.form['email'] == "":
-            flash("Email vide, veuillez ressaisir un email")
-            return render_template('index.html')
-        clubs_list = []
-        for club in clubs:
-            if club['email'] == request.form['email']:
-                clubs_list.append(club)
-                return render_template(
-                    'welcome.html',
-                    club=club,
-                    competitions=competitions)
-        if clubs_list == []:
-            flash('Utilisateur non reconnu')
-            return render_template('index.html')
 
-    @app.route('/book/<competition>/<club>')
-    def book(competition, club):
-        found_club = [c for c in clubs if c['name'] == club][0]
-        found_competition = [c for c in competitions if c['name'] == competition][0]
-        if found_club and found_competition:
-            return render_template(
-                'booking.html', club=found_club, competition=found_competition)
-        else:
-            flash("Something went wrong-please try again")
-            return render_template(
-                'welcome.html', club=club, competitions=competitions)
-
-    @app.route('/purchase_places', methods=['POST'])
-    def purchase_places():
-        competition = [
-            c for c in competitions if c['name'] == request.form['competition']][0]
-        club = [c for c in clubs if c['name'] == request.form['club']][0]
-        places_required = int(request.form['places'])
-        competition['number_of_places'] = int(
-            competition['number_of_places'])-places_required
-        flash('Great-booking complete!')
+@app.route('/book/<competition>/<club>')
+def book(competition, club):
+    found_club = [c for c in clubs if c['name'] == club][0]
+    found_competition = [c for c in competitions if c['name'] == competition][0]
+    if found_club and found_competition:
+        return render_template(
+            'booking.html', club=found_club, competition=found_competition)
+    else:
+        flash("Something went wrong-please try again")
         return render_template(
             'welcome.html', club=club, competitions=competitions)
 
-    # TODO: Add route for points display
-    @app.route('/logout')
-    def logout():
-        return redirect(url_for('index'))
 
-    return app
+@app.route('/purchase_places', methods=['POST'])
+def purchase_places():
+    competition = [c for c in competitions if c['name'] == request.form['competition']][0]
+    club = [c for c in clubs if c['name'] == request.form['club']][0]
+    places_required = int(request.form['places'])
+    print(type(competition['number_of_places']))
+    competition['number_of_places'] = int(competition['number_of_places'])-places_required
+    flash('Great-booking complete!')
+    return render_template(
+       'welcome.html', club=club, competitions=competitions)
 
 
-app = create_app({"TESTING": False})
+# TODO: Add route for points display
+@app.route('/logout')
+def logout():
+    return redirect(url_for('index'))
+
 
 if __name__ == "__main__":
     app.run()
